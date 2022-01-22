c94c330326586010f3daa5c7503780b1f0a23745
nuke2015 <196971567@qq.com>
2022-01-22 21:57:28 +0800

    增加一个crm_helper,用于管理后台,登陆 助手.

---
+++ src/org/crm_helper.php
@@ -0,0 +1,127 @@
+<?php
+namespace nuke2015\api\org;
+
+// 后台管理员登陆助手
+class crm_helper
+{
+
+    //  $config = [
+    //     'url_api'  => '',
+    //     'url_jump' => '',
+    // ];
+    public static function check_login($config)
+    {
+        // 首次登陆
+        if (isset($_REQUEST['admin_id']) && $_REQUEST['token']) {
+            $admin_id = intval($_REQUEST['admin_id']);
+            $token    = trim($_REQUEST['token']);
+            // 校验并登陆
+            $req = ['methodName' => 'AdminInfo', 'admin_id' => $admin_id, 'token' => $token];
+            $txt = myhttp::curl($config['url_api'], 'get', $req);
+            if ($txt) {
+                $info = json_decode($txt, 1);
+                if ($info && count($info)) {
+                    return [intval($info['code']), $info['data']];
+                } else {
+                    return [3, '登陆网关数据异常!'];
+                }
+            } else {
+                return [2, '网络连接不上!'];
+            }
+        } else {
+            return [1, '请用微信扫码登陆!'];
+        }
+    }
+
+    // 显示微信登陆码
+    public static function show_login_code($config)
+    {
+        echo <<<doc
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>管理员登陆入口</title>
+        <script src="https://apps.bdimg.com/libs/jquery/2.1.1/jquery.min.js"></script>
+    </head>
+    <body
+        style="width: 100%; background-color: rgb(140, 197, 255); background-image: url(/Public/union/login-bg.jpg); background-repeat: no-repeat; background-size: cover;">
+        <center>
+            <div class="code">
+                <h1>请用微信扫码登陆!</h1>
+                <img src="#" id="qrcode" alt="" style="width:215px;height:215px;">
+            </div>
+        </center>
+        <script language="javascript">
+            api = {
+                seed: '',
+                host: "{$config['url_api']}",
+                randomWord: function(randomFlag, min, max) {
+                    var str = "",
+                        range = min,
+                        arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                            'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+                        ];
+                    // 随机产生
+                    if (randomFlag) {
+                        range = Math.round(Math.random() * (max - min)) + min;
+                    }
+                    for (var i = 0; i < range; i++) {
+                        pos = Math.round(Math.random() * (arr.length - 1));
+                        str += arr[pos];
+                    }
+                    return str;
+                },
+                SeedCode: function() {
+                    api.seed = api.randomWord(0, 32);
+                    var req = {
+                        "methodName": "SeedCode",
+                        "seed": api.seed,
+                    }
+                    api.ajax(req, function(res) {
+                        api.checkError(res);
+                        var qrcode = res.data.qrcode;
+                        $("#qrcode").attr("src", qrcode);
+                    })
+                },
+                SeedCheck: function() {
+                    var req = {
+                        "methodName": "SeedCheck",
+                        "seed": api.seed
+                    }
+                    api.ajax(req, function(res) {
+                        console.log(res);
+                        if (res.code > 0) {
+                            alert(res.msg);
+                        } else {
+                            if (res.code == 0 && res.data.admin_id) {
+                                location.href = "{$config['url_jump']}&admin_id=" + res.data.admin_id +
+                                    "&token=" + res.data.token;
+                            }
+                        }
+                    });
+                },
+                ajax: function(req, res) {
+                    $.post(api.host, req, res);
+                },
+                checkError: function(res) {
+                    console.info(res.data);
+                }
+            }
+            var init = (function() {
+                api.SeedCode();
+                // 每秒一次
+                setInterval(api.SeedCheck, 1000);
+                //30秒后不查
+                clearInterval(api.SeedCheck, 30000);
+            })()
+        </script>
+
+    </body>
+</html>
+
+doc;
+    }
+}
